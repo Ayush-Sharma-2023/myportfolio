137c255251ae4d43b8b7995d1a3ead62dd61c87d
Ayush-Sharma-2023 <Ayushsharmaarps@gmail.com>
2024-07-09 23:32:50 +0530

    created Marquee

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,8 +1,19 @@
 import Calculator from "./../images/HiTechCalculator.png";
+import Marquee from "react-fast-marquee"
+
 import "./projects.css";
 
 
 export default function Projects() {
+
+    const handleMouseOver = (e) => {
+        e.currentTarget.classList.add('paused'); // Pause animation
+    };
+
+    const handleMouseOut = (e) => {
+        e.currentTarget.classList.remove('paused'); // Resume animation
+    };
+
     return(
         <>
 
@@ -10,10 +21,35 @@ export default function Projects() {
         
 
         <div className="Projects">
+            <Marquee pauseOnHover speed={200}>
+            <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+            <img className="calculator" src={Calculator} alt="HiTech Calculator" />
+            </a>
+            <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+            <img className="calculator" src={Calculator} alt="HiTech Calculator" />
+            </a>
+            <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+            <img className="calculator" src={Calculator} alt="HiTech Calculator" />
+            </a>
+            <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+            <img className="calculator" src={Calculator} alt="HiTech Calculator" />
+            </a>
+            <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+            <img className="calculator" src={Calculator} alt="HiTech Calculator" />
+            </a>
+            <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+            <img className="calculator" src={Calculator} alt="HiTech Calculator" />
+            </a>
 
-        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
-    <img className="calculator" src={Calculator} alt="HiTech Calculator" />
-</a>
+    
+
+
+            </Marquee>
+            {/* <marquee behavior="scroll" direction="left" scrollamount="25" onMouseOver={this.stop} onMouseOut={this.start} > */}
+
+        
+            {/* N */}
+
 
         </div>
         
