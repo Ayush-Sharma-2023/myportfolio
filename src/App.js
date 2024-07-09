cacaeb9ca7a2b1339ad6ef1732b72a9128df7558
Ayush-Sharma-2023 <Ayushsharmaarps@gmail.com>
2024-07-10 00:16:32 +0530

    modified:   .github/workflows/deploy.yml 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Banner from './components/banner';
 import Navbar from './components/navbar';
 import Home from './components/home';
 
+
 function App() {
   return (
     <>
     
     {< Banner/>}
     {< Navbar/>}
+    {<Home />}
     
 
 
