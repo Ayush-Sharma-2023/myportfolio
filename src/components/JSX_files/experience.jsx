6f48e48dd2dfd46a0e1e23bfbfa988e75cf3c0aa
Ayush-Sharma-2023 <Ayushsharmaarps@gmail.com>
2024-07-20 20:45:45 +0530

    modified: src/components/JSX_files/experience.jsx

--- src/components/JSX_files/experience.jsx
+++ src/components/JSX_files/experience.jsx
@@ -9,11 +9,40 @@ export default function Experience() {
       <div className={style.timeline}>
 
       
-        <div className={style.container}>
-          <div className={style.jobD}>
+        {/* <div className={style.container}>
+          <div className={style.jobD1}>
+           <div className={style.content}>
+              <p className={style.role1}>Frontend Developer Intern</p>
+              <p className={style.duration1}>May 2024 - August 2024</p>
+
+              
+              
+                <p className={style.company1} >
+                <a className={style.company1} href="https://rnpsoft.framer.website/" target="_blank"> @Rnpsoft</a> </p>
+                <br />
+
+              <p className={style.jobDesc1}>
+                Worked in a team <br />
+                Worked on projects using React <br />
+                Utilized Git and GitHub for collaboration <br />
+                Deployed websites using GitHub
+              </p>
+            </div>
             <div className={style.pin}>
               <div className={style.dot}></div>
               <div className={style.Vline}></div>
+              
+            </div>
+          
+          </div>
+        </div> */}
+        
+        <div className={style.container}>
+          <div className={style.jobD2}>
+            <div className={style.pin}>
+              <div className={style.dot}></div>
+              <div className={style.Vline}></div>
+              
             </div>
             <div className={style.content}>
               <p className={style.role}>Frontend Developer Intern</p>
